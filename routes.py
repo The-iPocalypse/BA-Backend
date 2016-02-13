63be85b750b2be8c709820e3d8505920856f6419
Richer Larivière <richerlariviere@gmail.com>
2016-02-13 03:51:58 -0500

    Added unclean User method

--- routes.py
+++ routes.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import json
+
+from flask import Flask, Response
+import pymysql.cursors
 
 app = Flask(__name__)
 app.debug = True
@@ -6,4 +9,23 @@ app.debug = True
 
 @app.route('/')
 def home():
-    return "Hey ma gang de fous"
+    return 'API de iPocalypse'
+
+
+@app.route('/users', methods=['GET'])
+def users():
+    connection = pymysql.connect(host='159.203.27.0',
+                             user='root',
+                             password='mz2v6BqKyw',
+                             db='AMC',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT `id`, `username`, `name`, `picture`, `rating`, `description` FROM `Users`"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return Response(json.dumps(result, ensure_ascii=False, indent=2).encode('utf8'),  mimetype='application/json', content_type='application/json; charset=utf-8')
+    finally:
+        connection.close()
